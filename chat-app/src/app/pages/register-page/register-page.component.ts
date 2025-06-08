9e88d69bcbff5f5a8c8e746f43e17d842af0cd76
Jeziel Nogueira <jezielnogueira_15@yahoo.com.br>
2025-06-08 00:57:18 -0300

    init

---
+++ chat-app/src/app/pages/register-page/register-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-page',
+  imports: [],
+  templateUrl: './register-page.component.html',
+  styleUrl: './register-page.component.sass'
+})
+export class RegisterPageComponent {
+
+}
